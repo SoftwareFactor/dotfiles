dbd5f1df586f9134fa44d7264183d1cd3af0f564
Karolis <karolis.petreikis@gmail.com>
2015-08-14 18:43:58 +0300

    updated link.sh

--- link.sh
+++ link.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles/old             # old dotfiles backup directory
-files=".vim .tmux.conf"    # list of files/folders to symlink in homedir
+files="vim tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
